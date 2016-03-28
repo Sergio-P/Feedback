5658ed789718dcc2bff5b34eb1fb3a2d9c15ca30
Sergio <elxsergio@gmail.com>
2016-03-28 00:20:46 -0300

    Modularization support

--- server.js
+++ server.js
@@ -2,14 +2,11 @@
 var express = require('express');
 var session = require('express-session');
 var restpg = require("./rest-pg.js");
-//var http = require('http');
 
 var app = module.exports = express();
 var conString = require("./passwords.js")("conString");
 
 var port = 8502;
-
-//http.createServer(app);
 
 app.use(express.static(__dirname+'/resources'));
 app.set('view engine', 'ejs');
@@ -18,7 +15,9 @@ app.use(session({secret: 'ssshhh', saveUninitialized: false, resave: false}));
 
 var ses;
 
+if(!module.parent){
+    app.listen(port,function(){
+        console.log("Listening at port "+port+"\n Ctrl + C to shut down");
+    });
+}
 
-app.listen(port,function(){
-    console.log("Listening at port "+port+"\n Ctrl + C to shut down");
-});
