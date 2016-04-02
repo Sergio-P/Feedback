9da9fbf7991cfbb114461ae9588261d86f5bfefc
Sergio <elxsergio@gmail.com>
2016-04-02 16:49:39 -0300

    Session list and DB config

---
+++ DBConfig.sql
@@ -0,0 +1,58 @@
+create database feedback;
+create extension postgis;
+
+create table users (
+    id  serial,
+    name text not null,
+    pass text,
+    mail text,
+    primary key (id)
+);
+
+create table sessions (
+    id  serial,
+    name text not null,
+    descr text,
+    time timestamp with time zone,
+    creator integer,
+    code varchar(6),
+    primary key (id),
+    foreign key(creator) references users(id)
+);
+
+create table sesusers (
+    sesid integer,
+    uid integer,
+    foreign key(sesid) references sessions(id),
+    foreign key(uid) references users(id)
+);
+
+create table feeds (
+    id serial,
+    descr varchar(160) not null,
+    time timestamp with time zone,
+    author integer,
+    geom geometry,
+    parentfeed integer,
+    sesid integer,
+    primary key(id),
+    foreign key(author) references users(id),
+    foreign key(parentfeed) references feeds(id),
+    foreign key(sesid) references sessions(id)
+);
+
+create table pictures (
+    path text not null,
+    fid integer,
+    foreign key(fid) references feeds(id)
+);
+
+create table history (
+    id serial,
+    query text not null,
+    uid integer,
+    sesid integer,
+    primary key(id),
+    foreign key(uid) references users(id),
+    foreign key(sesid) references sessions(id)
+);
---
+++ resources/js/seslistapp.js
@@ -0,0 +1,15 @@
+var app = angular.module("SesList",["ui.bootstrap"]);
+
+app.controller("SesListController",function($scope){
+
+    var self = $scope;
+
+    self.newsesopen = false;
+
+    self.sessions = [
+        {id:1, name:"Ses 1", descr:"Una descripcion muy larga"},
+        {id:2, name:"Ses 2", descr:"Una descripcion muy largadssd"},
+        {id:3, name:"Ses 3", descr:"Una descripcdsaasassda"}
+    ];
+
+});
