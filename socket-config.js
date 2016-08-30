547eb54aea4d198f34e432cabdee93a4c9ae0458
Sergio-P <elxsergio@gmail.com>
2016-08-29 23:05:22 -0300

    Minor changes

--- socket-config.js
+++ socket-config.js
@@ -1,6 +1,7 @@
 module.exports.configSocket = function(io){
     module.exports.updMsg = function(){
-	console.log(io.of("/Feedback"));
+        console.log(io);
+	    console.log(io.of("/Feedback"));
         io.of("/Feedback").emit("upd",{});
     };
 };
