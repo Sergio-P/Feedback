1dc386801f1c39b4fd4ebd808c4d86482b853ea1
Sergio-P <elxsergio@gmail.com>
2016-09-27 22:43:51 -0300

    Added location button in map

--- resources/js/app.js
+++ resources/js/app.js
@@ -381,6 +381,8 @@ app.controller("MapController",function($scope){
         });
         self.drawingManager.setMap(self.map);
         google.maps.event.addListener(self.drawingManager,'overlaycomplete',self.overlayHandler);
+        self.setLoctionMapCenter();
+        self.createLocationButton();
     };
 
     self.overlayHandler = function(event){
@@ -509,10 +511,45 @@ app.controller("MapController",function($scope){
         };
     };
 
-    navigator.geolocation.getCurrentPosition(function(pos){
-        var geolocation = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
-        self.map.setCenter(geolocation);
-    });
+    self.setLoctionMapCenter = () => {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            var geolocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+            self.map.setCenter(geolocation);
+        });
+    };
+
+    self.createLocationButton = () => {
+        var controlDiv = document.createElement('div');
+
+        var firstChild = document.createElement('button');
+        firstChild.style.backgroundColor = '#fff';
+        firstChild.style.border = 'none';
+        firstChild.style.outline = 'none';
+        firstChild.style.width = '28px';
+        firstChild.style.height = '28px';
+        firstChild.style.borderRadius = '2px';
+        firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+        firstChild.style.cursor = 'pointer';
+        firstChild.style.marginRight = '10px';
+        firstChild.style.padding = '0';
+        firstChild.title = 'Your Location';
+        controlDiv.appendChild(firstChild);
+
+        var secondChild = document.createElement('div');
+        secondChild.style.margin = '5px';
+        secondChild.style.width = '18px';
+        secondChild.style.height = '18px';
+        secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)';
+        secondChild.style.backgroundSize = '180px 18px';
+        secondChild.style.backgroundPosition = '0 0';
+        secondChild.style.backgroundRepeat = 'no-repeat';
+        firstChild.appendChild(secondChild);
+
+        firstChild.addEventListener('click', self.setLoctionMapCenter);
+
+        controlDiv.index = 1;
+        self.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+    };
 
     self.init();
     self.shared.updateMap = self.updateMap;
